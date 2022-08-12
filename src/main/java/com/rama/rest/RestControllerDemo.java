09afc6def790116290926f475acadf90950e075e
Ramakotaiah10 <ram10kvrc@gmail.com>
2022-08-12 23:55:12 +0530

    First commit

---
+++ src/main/java/com/rama/rest/RestControllerDemo.java
@@ -0,0 +1,15 @@
+package com.rama.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestControllerDemo {
+
+	@GetMapping("welcome/{name}")
+	public String hello(@PathVariable String name)
+	{
+		return "Hello "+name+" Welcome to Spring boot Rest with Jenkins...!!";
+	}
+}
