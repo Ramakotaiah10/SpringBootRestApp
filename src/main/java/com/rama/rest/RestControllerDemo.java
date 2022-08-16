d3bb131026ba81a5ca3da20a028f3501d0888570
Ramakotaiah10 <ram10kvrc@gmail.com>
2022-08-16 10:10:51 +0530

    message changed

--- src/main/java/com/rama/rest/RestControllerDemo.java
+++ src/main/java/com/rama/rest/RestControllerDemo.java
@@ -10,6 +10,7 @@ public class RestControllerDemo {
 	@GetMapping("welcome/{name}")
 	public String hello(@PathVariable String name)
 	{
-		return "Hi "+name+" Welcome to Spring boot Rest with Jenkins...!!";
+	    String msg="Hi Bhanu how are ...";
+		return "Hi "+msg+" Welcome to Spring boot Rest with Jenkins...!!";
 	}
 }
