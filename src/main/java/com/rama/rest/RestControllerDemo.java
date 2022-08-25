c0e2a094e188beb077db7a7a4e289f3f3c45f45b
Ramakotaiah <81919215+Ramakotaiah10@users.noreply.github.com>
2022-08-25 11:18:47 +0530

    Update RestControllerDemo.java

--- src/main/java/com/rama/rest/RestControllerDemo.java
+++ src/main/java/com/rama/rest/RestControllerDemo.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RestControllerDemo {
 
+	
+	
 	@GetMapping("welcome/{name}")
 	public String hello(@PathVariable String name)
 	{
